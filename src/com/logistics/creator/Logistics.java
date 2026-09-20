7452c1e908c2a52844115180b50a4e15c0db9e99
muminamukazhan <mukazhan13@gmail.com>
2026-09-20 22:49:05 +0500

    Add abstract Logistics creator

---
+++ src/com/logistics/creator/Logistics.java
@@ -0,0 +1,13 @@
+package com.logistics.creator;
+
+import com.logistics.transport.Transport;
+
+public abstract class Logistics {
+
+    protected abstract Transport createTransport();
+
+    public void planDelivery(String cargo, String destination) {
+        Transport transport = createTransport();
+        transport.deliver(cargo, destination);
+    }
+}
