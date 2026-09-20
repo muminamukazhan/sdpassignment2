b9ccf1ce99ae1df342175515a5b3e367c4fb99e9
muminamukazhan <mukazhan13@gmail.com>
2026-09-20 23:05:53 +0500

    Add WindowsButton component

---
+++ src/com/gui/component/WindowsButton.java
@@ -0,0 +1,9 @@
+package com.gui.component;
+
+public class WindowsButton implements Button {
+
+    @Override
+    public void paint() {
+        System.out.println("Rendering Windows button");
+    }
+}
