7f70af365fc821f14a6d9da32ae21daaad71d909
muminamukazhan <mukazhan13@gmail.com>
2026-09-20 23:07:01 +0500

    Add MacOSCheckbox component

---
+++ src/com/gui/component/MacOSCheckbox.java
@@ -0,0 +1,9 @@
+package com.gui.component;
+
+public class MacOSCheckbox implements Checkbox {
+
+    @Override
+    public void paint() {
+        System.out.println("Rendering macOS checkbox");
+    }
+}
